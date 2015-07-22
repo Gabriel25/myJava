a56c8d58b6aa55f32038111b3c64ca1985c70c9b
Gabriel25 <g00401@hotmail.com>
2015-07-22 22:28:57 +0100

    Career cups challenge - Even upercase odd reverse

---
+++ StringUpperReverse.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ * @author Gavriil Gavriilidis
+ *
+ */
+public class StringUpperReverse {
+
+	public static String oddsUpperEvenReverse(String s){
+		//put the words into an array using space as the delimiter and 
+		//don't omit trailing spaces
+		String[] words = s.split(" ", -1);
+		
+		//this will hold the number of the each word in the string 
+		int wordCount=0;
+		for(int i=0; i<words.length; i++){
+		   //don't take spaces into account when counting the words
+			//and if spaces are encountered do nothing
+			if(words[i].equals(""))
+				continue;
+			else
+				wordCount++;
+			//convert the odd words to upper-case
+			if((wordCount)%2!=0){
+				words[i] = words[i].toUpperCase();
+			}
+			//and reverse the even
+			else{
+				words[i] = reverse(words[i]);
+			}
+		}
+		StringBuilder sb = new StringBuilder("");
+		//replace the spaces that were lost since they were used as delimiters 
+		for(String word: words){
+			sb.append(word + " ");
+		}
+		//remove the last space that was not there initially
+		return sb.toString().substring(0,sb.toString().length()-1);
+	}
+	
+	//helper method to reverse the even strings
+	private static String reverse(String s)
+	{
+		if(s.length()==0)
+		return s;
+		else return s.charAt(s.length()-1) + reverse(s.substring(0,s.length()-1));
+	}
+	
+	public static void main(String args[]){
+		String s = " My  name    is Gabriel   and career  cup   challenges are awesome!   ";
+		System.out.println(oddsUpperEvenReverse(s));
+	}
+}
